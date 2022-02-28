0478c7640d8645c45a1ea9730f998ab04b019a03
thts0ustas <th.ts0ustas@gmail.com>
2022-02-28 15:13:04 +0200

    migrations-added reservations: basic get requests

--- routes/reservation.controller.js
+++ routes/reservation.controller.js
@@ -1,6 +1,44 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const router = express.Router();
 const db = require("../models");
-const { Screening, MovieOfTheMonth, Auditorium, Movie } = db.sequelize.models;
+const { Reservation, User, Screening } = db.sequelize.models;
+
+// User Reservations for a screening
+router.get("/users/:id", async (req, res) => {
+  const reservation = await Reservation.findOne(
+    {
+      where: {
+        [Op.and]: [
+          { user_id: req.params.id },
+          { screening_id: req.query.screening },
+        ],
+      },
+    },
+    { include: User }
+  );
+  res.json(reservation);
+});
+
+// All user Reservations
+router.get("/users/:id/all", async (req, res) => {
+  const reservation = await Reservation.findByPk(req.params.id, {
+    include: {
+      model: User,
+    },
+  });
+  res.json(reservation);
+});
+
+// Screening Reservations
+router.get("/screenings/:id", async (req, res) => {
+  const reservation = await Reservation.findAll({
+    where: {
+      screening_id: req.params.id,
+    },
+    include: [User, Screening],
+  });
+  res.json(reservation);
+});
 
 module.exports = router;
