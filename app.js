7787c894c6a6217ffb4c3e7ea2ed17b3b4647c4e
thts0ustas <th.ts0ustas@gmail.com>
2022-03-01 12:06:51 +0200

    migrations - added cors module for integration with front end

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors")
 
 const indexRouter = require("./routes/index");
 const moviesRouter = require("./routes/movies.controller");
@@ -11,12 +12,25 @@ const screeningsRouter = require("./routes/screenings.controller");
 const reviewRouter = require("./routes/review.controller");
 const reservationRouter = require("./routes/reservation.controller");
 
+// const whitelist = ['http://localhost:3000','http://localhost:4000/movies/create']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+
+
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors())
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
