bd5f48653510553cfb989bf03fdfccc812d1b028
thts0ustas <th.ts0ustas@gmail.com>
2022-03-03 18:12:01 +0200

    migrations - Fixes in models name-giving. Refactor ticket's functionality.

--- models/seat.js
+++ models/seat.js
@@ -2,13 +2,7 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Seat extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       Seat.hasOne(models.ReservedSeat, {
         foreignKey: {
           name: "seats_id",
@@ -23,7 +17,6 @@ module.exports = (sequelize, DataTypes) => {
   }
   Seat.init(
     {
-      // auditorium_id: DataTypes.INTEGEG
       row_letter: DataTypes.STRING,
       seat_num: DataTypes.INTEGER,
     },
