f6a5dc86b2492981beb7c495271c0dc9bfe95ab4
Athanasios Tsoustas <60882323+thTs0ustas@users.noreply.github.com>
2022-03-16 02:14:27 +0200

    migration fixes

--- migrations/20220227144745-create-screening.js
+++ migrations/20220227144745-create-screening.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Screenings', {
+    await queryInterface.createTable("screenings", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       auditorium_id: {
         type: Sequelize.INTEGER,
         require: true,
         references: {
-          model: "Auditorium",
+          model: "auditorium",
           key: "id",
         },
       },
@@ -25,25 +25,25 @@ module.exports = {
         },
       },
       movie_starts: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       movie_ends: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       movie_date: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Screenings');
-  }
+    await queryInterface.dropTable("Screenings");
+  },
 };
--- migrations/20220301221315-create-reserved-seats.js
+++ migrations/20220301221315-create-reserved-seats.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("ReservedSeats", {
+    await queryInterface.createTable("reservedSeats", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         require: true,
         references: {
-          model: "Reservations",
+          model: "reservations",
           key: "id",
         },
       },
@@ -20,7 +20,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         require: true,
         references: {
-          model: "Seats",
+          model: "seats",
           key: "id",
         },
       },
