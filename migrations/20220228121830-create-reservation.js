91bd5e098626e8de319bff87ecfe82d70b4c7863
thts0ustas <th.ts0ustas@gmail.com>
2022-04-25 12:35:49 +0300

    minor refactor

--- migrations/20220228121830-create-reservation.js
+++ migrations/20220228121830-create-reservation.js
@@ -38,7 +38,7 @@ module.exports = {
       },
     });
   },
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.dropTable("Reservations");
   },
 };
